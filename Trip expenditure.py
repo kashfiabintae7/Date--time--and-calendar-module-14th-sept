8c095667422e58020dc18ec4e543f14eb571a5d0
Kashfia <kashfiabintae@gmail.com>
2024-09-14 17:27:40 +0600

    tripppp

---
+++ Trip expenditure.py
@@ -0,0 +1,36 @@
+print("\033c")
+
+def hotel_cost(nights):
+    return 2500*nights
+
+def plane_ride_cost(city):
+    if city == "Sylhet":
+        return 1500
+    elif city == "Bandarban":
+        return 1250
+    elif city == "Cox":
+        return 1800
+    elif city == "STMartin":
+        return 2375
+    
+def rental_car_cost(days):
+    if days>=7:
+        return 800*days - 500
+    elif days>=3:
+        return 800*days - 250
+    else: 
+        return 800*days
+    
+def totalAmount (city, day):
+    return hotel_cost(day) + plane_ride_cost(city) + rental_car_cost(day)
+
+c = input("Where do you wnana go? [Sylhet/ Bandarban/ Cox/ STMartin]: ")
+d = int(input("How many days do you want to stay: "))
+
+total = totalAmount(c,d)
+
+print()
+print(f"Hotel Cost for {d} days : {hotel_cost(d)}")
+print(f"Plane Ride Cost for {d} days : {plane_ride_cost(c)}")
+print(f"Rental Car Cost for {d} days : {rental_car_cost(d)}")
+print(f"Total Cost for {d} days : {total}")
